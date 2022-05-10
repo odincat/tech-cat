7f41ce3d56a81fe9e0cc25d9908d4d07c1ffbdcd
odincat <70816143+odincat@users.noreply.github.com>
2022-05-10 22:39:16 +0200

    [cosmetic] Add progress bar to admin panel

--- apps/catmin/components/auth/adminshell/AdminShell.tsx
+++ apps/catmin/components/auth/adminshell/AdminShell.tsx
@@ -4,12 +4,14 @@ import { Alert } from "@mantine/core";
 import { SpotlightProvider } from "@mantine/spotlight"
 import { NotificationsProvider } from '@mantine/notifications';
 import { RiAlarmWarningFill, RiSearch2Line } from "react-icons/ri"
+import NextNProgress from "nextjs-progressbar";
 
 const AdminShell = (props: any) => {
 
     return(
         <SpotlightProvider actions={spotlightActions} searchIcon={<RiSearch2Line />} shortcut="mod + SPACE" searchPlaceholder="Search..." nothingFoundMessage="No results">
             <NotificationsProvider>
+                <NextNProgress />
                 <Navbar />
                 {props.children}
             </NotificationsProvider>
